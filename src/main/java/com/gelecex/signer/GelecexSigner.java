70347d1a46b8dc800c5491ae0cb34012175a6b59
obetron <erenbasaran@gmail.com>
2018-11-14 10:56:50 +0300

    Creating base maven project.

---
+++ src/main/java/com/gelecex/signer/GelecexSigner.java
@@ -0,0 +1,7 @@
+package com.gelecex.signer;
+
+/**
+ * Created by ebasaran on 14.11.2018.
+ */
+public class GelecexSigner {
+}
---
+++ src/test/java/com/gelece/signer/GelecexSignerTest.java
@@ -0,0 +1,7 @@
+package com.gelece.signer;
+
+/**
+ * Created by ebasaran on 14.11.2018.
+ */
+public class GelecexSignerTest {
+}
