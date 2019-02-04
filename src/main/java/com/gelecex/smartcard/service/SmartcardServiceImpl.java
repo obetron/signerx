27292bdf8e92d1c2336d607ff15b026a41cbe609
obetron <erenbasaran@gmail.com>
2019-02-04 12:44:29 +0300

    'Couldn't found smartcard for installed smartcard driver' case handled.

--- src/main/java/com/gelecex/smartcard/service/SmartcardServiceImpl.java
+++ src/main/java/com/gelecex/smartcard/service/SmartcardServiceImpl.java
@@ -3,12 +3,9 @@ package com.gelecex.smartcard.service;
 import com.gelecex.smartcard.exception.XMLParserException;
 import com.gelecex.smartcard.model.Smartcard;
 import com.gelecex.smartcard.utils.GelecexUtils;
+import org.apache.log4j.Logger;
 
-import javax.smartcardio.Card;
-import javax.smartcardio.CardException;
-import javax.smartcardio.CardTerminal;
-import javax.smartcardio.CardTerminals;
-import javax.smartcardio.TerminalFactory;
+import javax.smartcardio.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,18 +14,25 @@ import java.util.List;
  */
 public class SmartcardServiceImpl implements SmartcardService {
 
+    private Logger LOGGER = Logger.getLogger(SmartcardServiceImpl.class);
+
     @Override
     public List<Smartcard> readSmartcardToken() throws CardException, XMLParserException {
         TerminalFactory terminalFactory = TerminalFactory.getDefault();
         CardTerminals cardTerminals = terminalFactory.terminals();
         List<Smartcard> smartcardList = new ArrayList<>();
         for (CardTerminal cardTerminal : cardTerminals.list()) {
-            Card card = cardTerminal.connect("*");
-            String atrValue = GelecexUtils.byteToHex(card.getATR().getBytes());
-            SmartcardDao smartcardDao = new SmartcardXMLImpl();
-            Smartcard smartcard = smartcardDao.getSmartcard(atrValue);
-            if(smartcard != null) {
-                smartcardList.add(smartcard);
+            try {
+                Card card = cardTerminal.connect("*");
+                String atrValue = GelecexUtils.byteToHex(card.getATR().getBytes());
+                SmartcardDao smartcardDao = new SmartcardXMLImpl();
+                Smartcard smartcard = smartcardDao.getSmartcard(atrValue);
+                if (smartcard != null) {
+                    smartcardList.add(smartcard);
+                }
+            } catch(CardNotPresentException e) {
+                LOGGER.debug("No plugged card found for installed smartcard driver!");
+                continue;
             }
         }
         return smartcardList;
