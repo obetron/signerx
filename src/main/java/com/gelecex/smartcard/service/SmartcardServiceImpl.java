18d56d119c0653f3428fa78738fe4de7e2f01d17
obetron <erenbasaran@gmail.com>
2019-02-14 07:17:04 +0300

    Smartcard ATR value null check added.

--- src/main/java/com/gelecex/smartcard/service/SmartcardServiceImpl.java
+++ src/main/java/com/gelecex/smartcard/service/SmartcardServiceImpl.java
@@ -5,7 +5,12 @@ import com.gelecex.smartcard.model.Smartcard;
 import com.gelecex.smartcard.utils.GelecexUtils;
 import org.apache.log4j.Logger;
 
-import javax.smartcardio.*;
+import javax.smartcardio.Card;
+import javax.smartcardio.CardException;
+import javax.smartcardio.CardNotPresentException;
+import javax.smartcardio.CardTerminal;
+import javax.smartcardio.CardTerminals;
+import javax.smartcardio.TerminalFactory;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,11 +29,15 @@ public class SmartcardServiceImpl implements SmartcardService {
         for (CardTerminal cardTerminal : cardTerminals.list()) {
             try {
                 Card card = cardTerminal.connect("*");
-                String atrValue = GelecexUtils.byteToHex(card.getATR().getBytes());
-                SmartcardDao smartcardDao = new SmartcardXMLImpl();
-                Smartcard smartcard = smartcardDao.getSmartcard(atrValue);
-                if (smartcard != null) {
-                    smartcardList.add(smartcard);
+                if(card.getATR() != null) {
+                    String atrValue = GelecexUtils.byteToHex(card.getATR().getBytes());
+                    SmartcardDao smartcardDao = new SmartcardXMLImpl();
+                    Smartcard smartcard = smartcardDao.getSmartcard(atrValue);
+                    if (smartcard != null) {
+                        smartcardList.add(smartcard);
+                    }
+                } else {
+                    LOGGER.error("Smartcard ATR value could not read! - " + cardTerminal.getName());
                 }
             } catch(CardNotPresentException e) {
                 LOGGER.debug("No plugged card found for installed smartcard driver!");
