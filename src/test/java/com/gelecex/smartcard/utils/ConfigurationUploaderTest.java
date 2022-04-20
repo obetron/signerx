1483e348828a012af18023a98fb6d993d75aa055
obetron <erenbasaran@gmail.com>
2022-04-20 21:38:08 +0300

    importlar duzenlendi.

--- src/test/java/com/gelecex/smartcard/utils/ConfigurationUploaderTest.java
+++ src/test/java/com/gelecex/smartcard/utils/ConfigurationUploaderTest.java
@@ -1,9 +1,6 @@
 package com.gelecex.smartcard.utils;
 
-import com.gelecex.smartcard.exception.GelecexSignerException;
 import org.apache.log4j.Logger;
-import org.junit.Assert;
-import org.junit.Test;
 
 /**
  * Created by obetron on 17.11.2018
