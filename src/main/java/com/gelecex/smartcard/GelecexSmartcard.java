de59bc670704b2edccab1d87ec43a8cd36e00ca4
obetron <erenbasaran@gmail.com>
2022-04-20 22:04:50 +0300

    silinen dosyalarin path leri gitignore a eklendi.

--- src/main/java/com/gelecex/smartcard/GelecexSmartcard.java
+++
@@ -1,7 +0,0 @@
-package com.gelecex.smartcard;
-
-/**
- * Created by obetron on 18.12.2018
- */
-public class GelecexSmartcard {
-}
--- src/main/java/com/gelecex/smartcard/controller/SmartcardController.java
+++
@@ -1,7 +0,0 @@
-package com.gelecex.smartcard.controller;
-
-/**
- * Created by obetron on 18.12.2018
- */
-public class SmartcardController {
-}
