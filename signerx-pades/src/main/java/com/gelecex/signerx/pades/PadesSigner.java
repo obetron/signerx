a00734fc33dad62bfc07aefa27cf1525ddaa259d
obetron <erenbasaran@gmail.com>
2022-06-07 07:21:39 +0300

    pades imza detaylarini tutacak padessignature model sinifi olusturuldu.

--- signerx-pades/src/main/java/com/gelecex/signerx/pades/PadesSigner.java
+++ signerx-pades/src/main/java/com/gelecex/signerx/pades/PadesSigner.java
@@ -1,11 +1,20 @@
 package com.gelecex.signerx.pades;
 
 import com.gelecex.signerx.common.signature.BaseSigner;
+import com.gelecex.signerx.pades.model.PadesSignature;
 
 /**
  * Created by obetron on 6.06.2022
  */
 public class PadesSigner implements BaseSigner {
+
+    private PadesSignature padesSignature;
+
+    private PadesSigner(){}
+
+    public PadesSigner(PadesSignature padesSignature) {
+        this.padesSignature = padesSignature;
+    }
 
     @Override
     public byte[] createSignature() {
---
+++ signerx-pades/src/main/java/com/gelecex/signerx/pades/model/PadesSignature.java
@@ -0,0 +1,71 @@
+package com.gelecex.signerx.pades.model;
+
+/**
+ * Created by obetron on 7.06.2022
+ */
+public class PadesSignature {
+
+    private String name;
+    private String location;
+    private String reason;
+    private String contanctInfo;
+    private String signatureDate;
+    private byte[] content;
+    private byte[] signedContent;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getContanctInfo() {
+        return contanctInfo;
+    }
+
+    public void setContanctInfo(String contanctInfo) {
+        this.contanctInfo = contanctInfo;
+    }
+
+    public String getSignatureDate() {
+        return signatureDate;
+    }
+
+    public void setSignatureDate(String signatureDate) {
+        this.signatureDate = signatureDate;
+    }
+
+    public byte[] getContent() {
+        return content;
+    }
+
+    public void setContent(byte[] content) {
+        this.content = content;
+    }
+
+    public byte[] getSignedContent() {
+        return signedContent;
+    }
+
+    public void setSignedContent(byte[] signedContent) {
+        this.signedContent = signedContent;
+    }
+}
