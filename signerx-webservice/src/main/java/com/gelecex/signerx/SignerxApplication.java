bcd5b106b3b401fed6568a3ea18f670e17c134f4
obetron <erenbasaran@gmail.com>
2023-02-11 23:02:42 +0300

    v0.0.3: OpenFeign implementation excluded.

--- signerx-webservice/src/main/java/com/gelecex/signerx/SignerxApplication.java
+++ signerx-webservice/src/main/java/com/gelecex/signerx/SignerxApplication.java
@@ -2,14 +2,12 @@ package com.gelecex.signerx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
-@EnableFeignClients
 public class SignerxApplication {
     public static void main( String[] args ) {
         SpringApplication.run(SignerxApplication.class, args);
