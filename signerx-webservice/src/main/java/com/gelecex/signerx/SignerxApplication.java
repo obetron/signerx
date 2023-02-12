a269c9c259c74c10d2384521ecb46e4cac33ab2e
obetron <erenbasaran@gmail.com>
2023-02-12 13:35:39 +0300

    v0.0.3: version number fixed as 0.0.x pattern

--- signerx-webservice/src/main/java/com/gelecex/signerx/SignerxApplication.java
+++ signerx-webservice/src/main/java/com/gelecex/signerx/SignerxApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Hello world!
- *
+ * @author obetron
+ * @date 11.02.2023 - 22:27
  */
 @SpringBootApplication
 public class SignerxApplication {
