039f4f5ccedcdc44b07fa0033322af9b62ad8fd5
obetron <erenbasaran@gmail.com>
2023-12-11 23:33:53 +0300

    v0.0.5: signerx-pkcs11 separated from main project.

--- signerx-common/src/main/java/com/gelecex/signerx/common/EnumOsArch.java
+++
@@ -1,8 +0,0 @@
-package com.gelecex.signerx.common;
-
-/**
- * Created by obetron on 30.04.2022
- */
-public enum EnumOsArch {
-    x32, x64
-}
--- signerx-common/src/main/java/com/gelecex/signerx/common/EnumOsName.java
+++
@@ -1,8 +0,0 @@
-package com.gelecex.signerx.common;
-
-/**
- * Created by obetron on 30.04.2022
- */
-public enum EnumOsName {
-    Windows, Linux, Mac;
-}
